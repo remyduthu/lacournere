3ce2d73881905123ecd84b5ba730c7f1b925ba16
Remy DUTHU <remy.duthu@gmail.com>
2021-11-20 11:41:55 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "media",
+  theme: {
+    extend: {
+      backgroundImage: {
+        landing: "url('../images/landing.jpg')",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
